c86f4735a60afa1b82b75c4be9e7a2d7fe6ca4b2
hzyhxd98 <58780132+hzyhxd98@users.noreply.github.com>
2023-11-03 00:42:48 +0800

    Update platform.sh

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -1,39 +1,56 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
-RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
-
-# The TP-Link bootloader gets its flash layout from "bootargs".
-# Use this to our advantage, and:
-#  1. Disable the second rootfs (usrimg2)
-#  2. Extend the first rootfs (usrimg1) to include more space
-#  3. Increase the baudrate to the expected 115200
-tplink_sg2xxx_fix_mtdparts() {
-	local args
-	args="bootargs mtdparts=spi_flash:896K(boot),128K(env),6144K(sys)"
-	args="$args,20480K(usrimg1@main),0K(usrimg2)"
-	args="$args,4096K(usrappfs),1024K(para)\n"
-	args="$args baudrate 115200"
-
-	echo -e "$args" | fw_setenv --script -
-}
-
 platform_check_image() {
 	return 0
+}
+
+platform_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	mikrotik,rb750gr3|\
+	mikrotik,rbm11g|\
+	mikrotik,rbm33g)
+		[ -z "$(rootfs_type)" ] && mtd erase firmware
+		;;
+	esac
+}
+
+platform_nand_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	ubnt-erx|\
+	ubnt-erx-sfp)
+		platform_upgrade_ubnt_erx "$ARGV"
+		;;
+	esac
 }
 
 platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	tplink,sg2008p-v1|\
-	tplink,sg2210p-v3)
-		tplink_sg2xxx_fix_mtdparts
-		default_do_upgrade "$1"
+	hc5962|\
+	r6220|\
+	netgear,r6350|\
+	ubnt-erx|\
+	ubnt-erx-sfp|\
+	xiaomi,mir3g|\
+	xiaomi,mir3p)
+		nand_do_upgrade "$ARGV"
+		;;
+	tplink,c50-v4)
+		MTD_ARGS="-t romfile"
+		default_do_upgrade "$ARGV"
 		;;
 	*)
-		default_do_upgrade "$1"
+		default_do_upgrade "$ARGV"
 		;;
 	esac
 }
