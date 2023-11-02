13d41e5c25076c8c3f2fe9fc2e2c5f5d2743e90b
hzyhxd98 <58780132+hzyhxd98@users.noreply.github.com>
2023-11-03 00:43:22 +0800

    Update ubnt.sh

--- target/linux/ramips/base-files/lib/upgrade/ubnt.sh
+++ target/linux/ramips/base-files/lib/upgrade/ubnt.sh
@@ -1,39 +1,76 @@
-PART_NAME=firmware
-REQUIRE_IMAGE_METADATA=1
+#
+# Copyright (C) 2015 OpenWrt.org
+#
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
-RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+. /lib/functions.sh
+#Note: this code also uses some functions from nand.sh, but it is expected to be run by nand.sh, so we are not
+#sourcing it explicitly here
 
-# The TP-Link bootloader gets its flash layout from "bootargs".
-# Use this to our advantage, and:
-#  1. Disable the second rootfs (usrimg2)
-#  2. Extend the first rootfs (usrimg1) to include more space
-#  3. Increase the baudrate to the expected 115200
-tplink_sg2xxx_fix_mtdparts() {
-	local args
-	args="bootargs mtdparts=spi_flash:896K(boot),128K(env),6144K(sys)"
-	args="$args,20480K(usrimg1@main),0K(usrimg2)"
-	args="$args,4096K(usrappfs),1024K(para)\n"
-	args="$args baudrate 115200"
+UBNT_ERX_KERNEL_INDEX_OFFSET=160
 
-	echo -e "$args" | fw_setenv --script -
+ubnt_get_target_kernel() {
+	local factory_mtd=$1
+	local current_kernel_index=$(hexdump -s $UBNT_ERX_KERNEL_INDEX_OFFSET -n 1 -e '/1 "%X "' ${factory_mtd})
+
+	if [ $current_kernel_index == "0" ]; then
+		echo 'kernel2'
+	elif [ $current_kernel_index == "1" ]; then
+		echo 'kernel1'
+	fi
 }
 
-platform_check_image() {
-	return 0
+ubnt_update_target_kernel() {
+	local factory_mtd=$1
+	local kernel_part=$2
+
+	local new_kernel_index
+	if [ $kernel_part == "kernel1" ]; then
+		new_kernel_index="\x00"
+	elif [ $kernel_part == "kernel2" ]; then
+		new_kernel_index="\x01"
+	else
+		echo 'Unknown kernel image index' >&2
+		return 1
+	fi
+
+	if ! (echo -e $new_kernel_index | dd of=${factory_mtd} bs=1 count=1 seek=$UBNT_ERX_KERNEL_INDEX_OFFSET); then
+		echo 'Failed to update kernel bootup index' >&2
+		return 1
+	fi
 }
 
-platform_do_upgrade() {
-	local board=$(board_name)
+platform_upgrade_ubnt_erx() {
+	local factory_mtd=$(find_mtd_part factory)
+	if [ -z "$factory_mtd" ]; then
+		echo "cannot find factory partition" >&2
+		exit 1
+	fi
 
-	case "$board" in
-	tplink,sg2008p-v1|\
-	tplink,sg2210p-v3)
-		tplink_sg2xxx_fix_mtdparts
-		default_do_upgrade "$1"
-		;;
-	*)
-		default_do_upgrade "$1"
-		;;
-	esac
+	local kernel_part="$(ubnt_get_target_kernel ${factory_mtd})"
+	if [ -z "$kernel_part" ]; then
+		echo "cannot find factory partition" >&2
+		exit 1
+	fi
+
+	# This is a global defined in nand.sh, sets partition kernel will be flashed into
+	CI_KERNPART=${kernel_part}
+
+	#Remove volume possibly left over from stock firmware
+	local ubidev="$( nand_find_ubi "$CI_UBIPART" )"
+	if [ -z "$ubidev" ]; then
+		local mtdnum="$( find_mtd_index "$CI_UBIPART" )"
+		if [ -z "$mtdnum" ]; then
+			echo "cannot find ubi mtd partition $CI_UBIPART" >&2
+			exit 1
+		fi
+		ubiattach -m "$mtdnum"
+		sync
+		ubidev="$( nand_find_ubi "$CI_UBIPART" )"
+	fi
+	if [ -n "$ubidev" ]; then
+		local troot_ubivol="$( nand_find_volume $ubidev troot )"
+		[ -n "$troot_ubivol" ] && ubirmvol /dev/$ubidev -N troot || true
+	fi
+
+	ubnt_update_target_kernel ${factory_mtd} ${kernel_part} || exit 1
 }
