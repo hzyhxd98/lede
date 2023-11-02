483b6e04a43045d5f9d52ba1a2b2f13787f8dc05
hzyhxd98 <58780132+hzyhxd98@users.noreply.github.com>
2023-11-03 00:50:20 +0800

    Update 01_preinit_do_ramips.sh

--- target/linux/ramips/base-files/lib/preinit/01_preinit_do_ramips.sh
+++ target/linux/ramips/base-files/lib/preinit/01_preinit_do_ramips.sh
@@ -1,47 +1,9 @@
-PART_NAME=firmware
-REQUIRE_IMAGE_METADATA=1
+#!/bin/sh
 
-platform_check_image() {
-	local board=$(board_name)
+do_ramips() {
+	. /lib/ramips.sh
 
-	case "$board" in
-	wd,mybooklive)
-		mbl_do_platform_check "$1"
-		return $?;
-		;;
-	*)
-		return 0
-		;;
-	esac
+	ramips_board_detect
 }
 
-platform_do_upgrade() {
-	local board=$(board_name)
-
-	case "$board" in
-	wd,mybooklive)
-		mbl_do_upgrade "$1"
-		;;
-	meraki,mr24|\
-	meraki,mx60|\
-	netgear,wndap620|\
-	netgear,wndap660|\
-	netgear,wndr4700)
-		nand_do_upgrade "$1"
-		;;
-	*)
-		default_do_upgrade "$1"
-		;;
-	esac
-}
-
-platform_copy_config() {
-	local board=$(board_name)
-
-	case "$board" in
-	wd,mybooklive|\
-	wd,mybooklive-duo)
-		mbl_copy_config
-		;;
-	esac
-}
+boot_hook_add preinit_main do_ramips
